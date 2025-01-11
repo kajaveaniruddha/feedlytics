3f6fb07e1bd3230ab2f33b8267aa23cf7e0ef5c6
Aniruddha Kajave <aakajave@gmail.com>
2025-01-11 22:23:39 +0530

    feat: integrate SquareBackdrop into sign-in page for improved UI consistency

--- Next/src/app/(auth)/sign-in/page.tsx
+++ Next/src/app/(auth)/sign-in/page.tsx
@@ -1,3 +1,4 @@
+import { SquareBackdrop } from "../sign-up/page";
 import SSOAuth from "../sign-up/sso-auth";
 import FormSignIn from "./form-signin";
 import { IAuthType } from "@/types/ApiResponse"
@@ -5,6 +6,7 @@ import { IAuthType } from "@/types/ApiResponse"
 const Page: React.FC = () => {
   return (
     <section className=" w-full h-screen flex justify-center items-center">
+      <SquareBackdrop />
       <div className="w-full flex flex-col justify-center max-w-md -mt-10 ">
         <div className="flex flex-col custom-shadow bg-[#1C1C1C] z-10 rounded-lg p-8 border border-white/20">
           <SSOAuth authType="login" />
