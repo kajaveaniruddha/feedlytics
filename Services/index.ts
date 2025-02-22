d482892421ac5a0ffc30daba6c89bd5670127af9
Aniruddha Kajave <aakajave@gmail.com>
2025-02-23 01:04:40 +0530

    feat: use environment variable for server port configuration

--- Services/index.ts
+++ Services/index.ts
@@ -3,7 +3,7 @@ import { emailQueue, feedbackQueue } from "./src/queue";
 import "dotenv/config";
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.json());
 
