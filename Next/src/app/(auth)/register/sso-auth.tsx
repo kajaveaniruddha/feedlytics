e5f4d525a407dcec9a73823206a8881ea40fd01f
Aniruddha Kajave <aakajave@gmail.com>
2025-06-14 01:43:22 +0530

    g-auth

--- Next/src/app/(auth)/register/sso-auth.tsx
+++ Next/src/app/(auth)/register/sso-auth.tsx
@@ -37,12 +37,12 @@ const SSOAuth = ({ authType }: SSOAuthProps) => {
                     variant="outline"
                     className="w-full"
                     onClick={googleSignIn}
-                    disabled={true}
+                    disabled={loading}
                 >
                     <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
                         <path d="M12.545,10.239v3.821h5.445c-0.712,2.315-2.647,3.972-5.445,3.972c-3.332,0-6.033-2.701-6.033-6.032s2.701-6.032,6.033-6.032c1.498,0,2.866,0.549,3.921,1.453l2.814-2.814C17.503,2.988,15.139,2,12.545,2C7.021,2,2.543,6.477,2.543,12s4.478,10,10.002,10c8.396,0,10.249-7.85,9.426-11.748L12.545,10.239z" />
                     </svg>
-                    Work in progress 
+                    Google 
                     {loading && (
                         <svg className="animate-spin h-5 w-5 ml-2" viewBox="0 0 24 24" fill="none">
                             <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
